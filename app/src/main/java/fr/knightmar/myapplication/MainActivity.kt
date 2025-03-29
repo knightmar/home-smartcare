ca6e5221f77d97c150a033df0446666134d3183d
Maria Flesch <maria.flesch@epita.fr>
2025-03-29 15:24:06 +0100

    feat(home-page): logos + text

--- app/src/main/java/fr/knightmar/myapplication/MainActivity.kt
+++ app/src/main/java/fr/knightmar/myapplication/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,7 +24,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,6 +98,14 @@ fun HomeScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val colors = listOf(Color(0xFFD3F8E2), Color(0xFFE4C1F9), Color(0xFFEDCFB3), Color(0xFFF694C1), Color(0xFFF4F7BE), Color(0xFFA9DEF9))
+        val logos = listOf(R.drawable.logo_calendrier, R.drawable.logo_multimedia, R.drawable.logo_contact, R.drawable.logo_urgence, R.drawable.logo_med_store, R.drawable.logo_suivi_med)
+        val text_buttons = listOf("Calendrier", "Multimédia", "Contacts", "Urgence", "Med Store", "Suivi Med")
+        var index = 0
+
+        val fontFamily = FontFamily(Font(R.font.luciole_regular))
+
+
         for (rowIndex in 0 until 2) { // 3 columns
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -100,10 +118,31 @@ fun HomeScreen() {
                             .padding(8.dp)
                             .weight(1f)
                             .height(350.dp),
-                        shape = RectangleShape
+                        shape = RectangleShape,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colors[index],
+                            contentColor = Color.Black,
+                            disabledContainerColor = colors[index],
+                            disabledContentColor = Color.Black
+                        )
                     ) {
-                        Text(text = "Bouton ${rowIndex * 2 + columnIndex + 1}", fontSize = 18.sp)
+
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Image(
+                                painter = painterResource(id = logos[index]),
+                                contentDescription = "Button image",
+                                modifier = Modifier.size(200.dp)
+                            )
+
+                            Text(text = text_buttons[index],
+                                fontSize = 50.sp,
+                                modifier = Modifier.padding(top = 40.dp),
+                                fontFamily = fontFamily)
+                        }
+
                     }
+
+                    index++;
                 }
             }
         }
